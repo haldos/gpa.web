2f844f867a2998ed5ff9debeb93f8405300f752b
Haldo Sponton <haldos@fing.edu.uy>
2012-03-26 15:35:03 -0300

    modified:   publicaciones.php 	modified:   src/publicaciones.php

--- src/publicaciones.php
+++ src/publicaciones.php
@@ -7,7 +7,7 @@
 <?php
 
 // cambiar el apellido del autor a buscar.
-$option["author"]  = "Rocamora";
+$option["groups"]  = "gpa";
 
 // No tocar, a menos que sepa lo que hace.
 $option["display"] = "list";
